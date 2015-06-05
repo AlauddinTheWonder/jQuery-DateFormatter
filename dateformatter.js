0a82eb95d898828b0ba0a7362ae03555419ebb29
Alauddin Ansari <wonder_a7@yahoo.co.in>
2015-06-05 17:07:42 +0530

    Added functionality for no separator
    Implemented formatter to work even if no separator is give. Ex: 2311 -> 02/03/2011

--- dateformatter.js
+++ dateformatter.js
@@ -3,16 +3,17 @@
  * https://github.com/AlauddinTheWonder/jQuery-DateFormatter
  * Copyright (c) 2015 -	Alauddin Ansari
  * Licensed under the GNU license (http://opensource.org/licenses/gpl-license)
- * Version: 0.0.0-dev
- * Uses: $('#dateinput').dateformatter({placeholder:'mm/dd/yyyy'});
+ * Version: 0.0.1
+ * Uses: $('#dateinput').dateformatter({placeholder:'mm/dd/yyyy', allowTimestamp: false});
  */
 
 
 (function($){
 	$.fn.dateformatter = function(options){
 		var settings = $.extend({
-            placeholder: "mm/dd/yyyy",
-        }, options );
+			placeholder: "mm/dd/yyyy",
+			allowTimestamp: false,
+		}, options );
 
 		this.each(function() {
 			$(this).attr({'placeholder': settings.placeholder, 'maxlength' : 10});
@@ -35,7 +36,6 @@
 				var _splits = "";
 				var y, m, d;
 
-
 				if(_od.indexOf('/') > 0)
 				{
 					splitter = "/";
@@ -50,10 +50,37 @@
 				}
 				else
 				{
-					var dt = new Date(_od*1000);
-					console.log(dt);
 					splitter = "/";
-					_od = (dt.getMonth()+1) + splitter + dt.getDate() + splitter + dt.getFullYear();
+
+					if(settings.allowTimestamp)
+					{
+						var dt = new Date(_od*1000);
+						console.log(dt);
+						splitter = "/";
+						_od = (dt.getMonth()+1) + splitter + dt.getDate() + splitter + dt.getFullYear();
+					}
+					else
+					{
+						if(_od.toString().length == 4)
+						{
+							var _m = _od.substr(0, 1);
+							var _d = _od.substr(1, 1);
+							var _y = _od.substr(2, 2);
+						}
+						else if(_od.toString().length == 6)
+						{
+							var _m = _od.substr(0, 2);
+							var _d = _od.substr(2, 2);
+							var _y = _od.substr(4, 2);
+						}
+						else if(_od.toString().length == 8)
+						{
+							var _m = _od.substr(0, 2);
+							var _d = _od.substr(2, 2);
+							var _y = _od.substr(4, 4);
+						}
+						_od = _m + splitter + _d + splitter + _y;
+					}
 				}
 
 				if(splitter != "")
@@ -67,12 +94,20 @@
 						y = parseInt(_splits[2]);
 
 						// if year is mis-placed Ex. yyyy-mm-dd
-						if(y.toString().length < 4 && m.toString().length == 4)
+						if(y.toString().length == 2 && m.toString().length == 4)
 						{
 							var c = m;
 							m = d;
 							d = y;
 							y = c;
+						}
+						// if year is mentioned in two digits Ex. mm-dd-yy
+						else if(y.toString().length == 2 && m.toString().length <= 2)
+						{
+							if(y < 30)
+								y = parseInt("20" + y);
+							else
+								y = parseInt("19" + y);
 						}
 
 						// if month and date inter-changed Ex. dd-mm-yyyy
